e79a05371bf5e197cf22153e7043056bb9e9ea81
anthiya-r <anthiya.r@gmail.com>
2026-02-21 12:04:40 +0700

    fix: add debug logs

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,14 @@
-import { betterAuth } from "better-auth";
-import { prismaAdapter } from "better-auth/adapters/prisma";
-import { username } from "better-auth/plugins";
-import prisma from "./prisma";
+import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { username } from 'better-auth/plugins';
+import prisma from './prisma';
+
+console.log('SECRET:', process.env.BETTER_AUTH_SECRET);
+console.log('DB:', process.env.DATABASE_URL);
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
-    provider: "postgresql",
+    provider: 'postgresql',
   }),
   emailAndPassword: {
     enabled: true,
